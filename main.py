aa1cc1d3b11297e9c48ff40de052cb031f9890ba
jkalarikkal <jkalarikkal007@gmail.com>
2024-11-13 17:18:10 +0300

    main.py

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ def inorder(root):
         print(root.data)
         inorder(root.right)
 
+
+def postorder(root):
+    if root:
+        inorder(root.left)
+        inorder(root.right)
+        print(root.data)
+
+def preorder(root):
+    if root:
+        print(root.data)
+        inorder(root.left)
+        inorder(root.right)
+
+
 root = Node(100)
 root.left = Node(20)
 root.right= Node(30)
@@ -25,4 +39,15 @@ inorder(root)
 #inorder - l, d, r
 #40,20,70,100,90,30,400
 
+
+
+print("Preorder traversal")
+
+preorder(root)
+
+
+
+print("Postorder traversal")
+
+postorder(root)
 
